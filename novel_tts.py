18616bcb78debe640cd58bc2b088c620607e987c
SheiUn <me.sheiun@gmail.com>
2020-01-06 09:12:32 +0800

    feat: add some new features
    - add clean func to clean text data
    - change index to os.listdir

--- novel_tts.py
+++ novel_tts.py
@@ -1,18 +1,53 @@
+import os
+import re
+
 from gtts import gTTS
 
-index = 798
+
+def clean(text: str) -> str:
+    text = re.sub(r"（.+）", "", text)
+    return (
+        "".join(
+            filter(
+                lambda l: "作者" not in l
+                or "類別" not in l
+                or "用戶名" not in l
+                or "書名" not in l
+                or ("第" not in l and "章" not in l),
+                text.splitlines(),
+            )
+        )
+        .split("<a")[0]
+        .split("未完待續")[0]
+        .split("  請記住:飛翔鳥中文小說網")[0]
+        .split("手機站全新改版升級地址")[0]
+        .replace("<p/>", "")
+        .replace("“", " ")
+        .strip()
+    )
+
+
+files = iter(
+    sorted(
+        filter(lambda f: f.endswith(".txt"), os.listdir("texts/")),
+        key=lambda f: int(re.search(r"\d+", f).group()),
+    )
+)
+
+file = next(files)
 
 while True:
     try:
-        tts = gTTS(open(f"texts/{index}.txt").read(), lang="zh-tw")
-        tts.save(f"sounds/{index}.mp3")
+        tts = gTTS(clean(open(f"texts/{file}").read()), lang="zh-tw")
+        tts.save(f"sounds/{file.replace('.txt', '')}.mp3")
     except FileNotFoundError:
-        print(f"{index} Not Found")
+        print(f"{file} Not Found")
         break
-    except Exception:
-        print(f"{index} Break")
+    except Exception as e:
+        print(f"{file} Break, Cause: {e}")
     else:
-        index += 1
-        print(f"{index} Done")
+        print(f"{file} Done")
+        file = next(files)
+
 
 print("All Done")
